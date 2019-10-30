b9aa6daba12f43031e21c5748d5c509c1411a15d
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-10-30 20:52:08 +0000

    Added user authentication functionality

--- express_server.js
+++ express_server.js
@@ -4,12 +4,28 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+//Database - url
 const urlDatabase = {
   b2xVn2: "www.lighthouselabs.ca",
   "9sm5xK": "www.google.com"
 };
 
-//Function to generate random string
+//Database - Users
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+//***** HELPER FUNCTIONS  *****//
+//1. Function to generate random string
 const generateRandomString = () => {
   let result = "";
   const source = "abcdefghijklmnopqrstuvwxyz1234567890";
@@ -19,6 +35,34 @@ const generateRandomString = () => {
   return result;
 };
 
+//2.Function to check if we have email in users object.
+const emailFinder = (source, email) => {
+  for (const userId in source) {
+    if (source[userId].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//3 Function to check the password
+const passwordChecker = (source, password) => {
+  for (const userId in source) {
+    if (source[userId].password === password) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//4 Function to get the user
+const findUserString = (source, email) => {
+  for (const userId in source) {
+    if (source[userId].email === email) {
+      return source[userId].id;
+    }
+  }
+};
 //** using the body parser so that the data sending from browser will be made human readable as they are
 // being encoded from buffer to human readable code
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,7 +81,7 @@ app.get("/", (req, res) => {
 //Rendering the database into url_index file in views.
 app.get("/urls", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     urls: urlDatabase
   };
   //render mehthod takes in file name and data that we want to send!
@@ -46,7 +90,9 @@ app.get("/urls", (req, res) => {
 
 //Rendering the new URLS
 app.get("/urls/new", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
   res.render("urls_new", templateVars);
 });
 
@@ -60,7 +106,7 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -88,29 +134,63 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect("/urls");
 });
 
-//Handling the login route and setting the cookie.
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
-});
-
-//Handling the post route
+//Handling the logout post route
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
 //Registration page
 //Handling get request
 app.get("/register", (req, res) => {
-  let templateVars = { username: req.cookies["username"] };
+  let templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
   res.render("registration", templateVars);
 });
 
 //Handling post request for registration page
 app.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("Handling post request");
+  //Check = Handling the errors if any
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send("Username and password should not be empty");
+  } else if (emailFinder(users, req.body.email)) {
+    res.status(400).send("Already registered user");
+  } else {
+    let randomString = generateRandomString();
+    users[randomString] = {
+      id: randomString,
+      email: req.body.email,
+      password: req.body.password
+    };
+    //Setting a cookie of random string
+    res.cookie("user_id", randomString);
+    res.redirect("/urls");
+  }
+});
+
+//Serving the login page - GET
+app.get("/login", (req, res) => {
+  const templateVars = { user: users[req.cookies["user_id"]] };
+  res.render("login", templateVars);
+});
+
+//Handling the login route - POST and setting the cookie.
+app.post("/login", (req, res) => {
+  if (!emailFinder(users, req.body.email)) {
+    //User with email NOT found;
+    res.status(403).send("User not found");
+  } else {
+    if (!passwordChecker(users, req.body.password)) {
+      //password NOT matched
+      res.status(403).send("password did not match");
+    } else {
+      console.log(req.body);
+      const userCookie = findUserString(users, req.body.email);
+      res.cookie("user_id", userCookie);
+      res.redirect("/urls");
+    }
+  }
 });
 
 //Server to listen on ...
